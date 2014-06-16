438234a252985a8860f7f48e28239219da9be707
Aneesh Neelam <neelam.aneesh@gmail.com>
2014-06-16 16:57:45 +0530

    Small Changes index.js changes

--- routes/index.js
+++ routes/index.js
@@ -3,38 +3,36 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    var temp = "";
-    res.render('Main', { title: temp });
-});
-router.get('/register', function(req, res) {
-    var temp = "";
-    res.render('register', { title: temp });
-});
-router.get('/login', function(req, res) {
-    var temp = "";
-    res.render('login', { title: temp });
-});
-router.get('/prize', function(req, res) {
-    var temp = "";
-    res.render('prize', { title: temp });
-});
-router.get('/howtoplay', function(req, res) {
-    var temp = "";
-    res.render('howtoplay', { title: temp });
-});
-router.get('/rules', function(req, res) {
-    var temp = "";
-    res.render('rules&scoring', { title: temp });
-});
-router.get('/sponsor', function(req, res) {
-    var temp = "";
-    res.render('sponsors', { title: temp });
-});
-router.get('/players', function(req, res) {
-    var temp = "";
-    res.render('stats', { title: temp });
+    res.render('Main', { });
 });
 
+router.get('/register', function(req, res) {
+    res.render('register', { });
+});
+
+router.get('/login', function(req, res) {
+    res.render('login', { });
+});
+
+router.get('/prize', function(req, res) {
+    res.render('prize', { });
+});
+
+router.get('/howtoplay', function(req, res) {
+    res.render('howtoplay', { });
+});
+
+router.get('/rules', function(req, res) {
+    res.render('rules&scoring', { });
+});
+
+router.get('/sponsor', function(req, res) {
+    res.render('sponsors', { });
+});
+
+router.get('/players', function(req, res) {
+    res.render('stats', { });
+});
 
 router.get('/express', function (req, res) {
     // Pull from Database
