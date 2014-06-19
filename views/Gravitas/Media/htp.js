a61908e34edf39e25fb6bc1a33f8939ecab0b702
Aneesh Neelam <neelam.aneesh@gmail.com>
2014-06-19 23:16:20 +0530

    Deleted Gravitas ./views/Gravitas folder has been deleted It was restored from a previous commit

--- views/Gravitas/Media/htp.js
+++
@@ -1,9 +0,0 @@
-var i = 0;
-var menu = ["Media/bat.html", "Media/bowl.html", "Media/all.html", "Media/coaches.html"];
-function ch(i) {
-
-    document.getElementById("frlnk").src = menu[i];
-    document.getElementById("temp").id = "nottemp";
-    document.getElementsByName(i.toString())[0].id = "temp";
-
-}
--- views/Gravitas/RS/htp.js
+++
@@ -1,9 +0,0 @@
-var i = 0;
-var menu = ["RS/Scom.html", "RS/Score.html", "RS/ChngSqd.html"];
-function ch(i) {
-
-    document.getElementById("frlnk").src = menu[i];
-    document.getElementById("temp").id = "nottemp";
-    document.getElementsByName(i.toString())[0].id = "temp";
-
-}
