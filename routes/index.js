e029e824918b5117f55adff555b21c5b908d99e7
Kashish Singhal <singhal2.kashish@gmail.com>
2014-07-01 23:56:42 +0530

    Home page redirection, with a few modules,
    Aneesh - I have added 2 new modules to the package.json file, please do check if they are written correctly.
    Incase it does not work,
    please run the following commands
    
    npm install moment
    npm install emailjs

--- routes/index.js
+++ routes/index.js
@@ -6,79 +6,96 @@ var router = express.Router();
 var util = require('util');
 var fs = require('fs');
 var url = require('url');
+var AM = require('account-manager.js');
+var EM = require('email-dispatcher.js');
 /* GET home page. */
-router.get('/', function (req, res) {   //homepage, option for login and signup
-    res.render('Main', { });
+router.get('/', function (req, res) { // Login page, attempting auto-login
+// check if the user's credentials are saved in a cookie //
+    if (req.cookies.user == undefined || req.cookies.pass == undefined) {
+        res.redirect('/login');
+    } else {
+        // attempt automatic login //
+        AM.autoLogin(req.cookies.user, req.cookies.pass, function (o) {
+            if (o != null) {
+                req.session.user = o;
+                res.redirect('/home');
+            } else {
+                res.redirect('/login');
+            }
+        });
+    }
 });
+
+
+router.get('/login', function (req, res) {      // page to login
+
+    res.render('login', { });
+});
+
+
+router.get('/loginproceed', function (req, res) {      // page to retrieve login data - a dummy page
+    var username = req.query.username;
+    var password = req.query.password;
+    console.log(username);
+    console.log(password);
+    res.render('login', { });
+});
+
 
 router.get('/register', function (req, res) {   // page to register
     res.render('register', { });
 });
-/*
- router.get('/registerproceed', function (req, res) {   // page to retrieve register data - a dummy page
-    var server = http.createServer(function (req, res) {
 
-        var url_parts = url.parse(req.url, true);
-        console.log(url_parts);
 
-        if (url_parts.pathname == '/register')
-            getData(res, url_parts);
-        // res.render('register', { });
-    });
-});
-function getData(res, url_parts) {
-    var uname = url_parts.query.name;
-    var password = url_parts.query.password;
-    var confpassword = url_parts.query.confirm;
-    console.log(uname);
-    console.log(password);
-    console.log(confpassword);
-}
- Must be rewritten
- */
-router.get('/login', function (req, res) {      // page to login
-    res.render('login', { });
+router.get('/registerproceed', function (req, res) {   // page to retrieve register data - a dummy page
+
 });
 
-router.get('/login', function (req, res) {      // page to retrieve login data - a dummy page
-    res.render('login', { });
-});
 
 router.get('/prize', function (req, res) {       //// page to view prizes
     res.render('prize', { });
 });
 
+
 router.get('/howtoplay', function (req, res) {   // page to view prizes
     res.render('howtoplay', { });
 });
+
 
 router.get('/rules', function (req, res) {       // page to view rules
     res.render('rules&scoring', { });
 });
 
+
 router.get('/sponsor', function (req, res) {     // sponsors page
     res.render('sponsors', { });
 });
+
 
 router.get('/players', function (req, res) {     // page for all players, only available if he has no squad
     res.render('stats', { });
 });
 
+
 router.get('/squad', function (req, res) {      // page to view the 16 player squad of a particular user
     res.render('squad', { });
 });
+
 
 router.get('/team', function (req, res) {      // view the assigned playing 11 with options to change the playing 11
     res.render('team', { });
 });
 
+
 router.get('/matchday', function (req, res) {      // page to view his next match schedule and the opponent team
     res.render('matchday', { });
 });
 
+
 router.get('/leaderboard', function (req, res) {      // view the standings of his league
     res.render('leaderboard', { });
 });
+
 
 router.get('/forum', function (req, res) {      // page to login
     res.render('forum', { });
