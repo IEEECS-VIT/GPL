0665460383e7bfb84a4dad386b059ad294860e4f
Aneesh Neelam <neelam.aneesh@gmail.com>
2014-07-01 21:24:50 +0530

    Updated Dependencies Updated dependencies to latest npm versions Added a temporary favicion

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -14,10 +14,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(favicon());
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(cookieParser());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
