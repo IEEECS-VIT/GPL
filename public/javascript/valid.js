5b87175398f186ad0960d5834e8440990ab33c48
BurnUalive <burnualive@hotmail.com>
2014-08-03 22:44:20 +0530

    minor stuff

--- public/javascript/valid.js
+++ public/javascript/valid.js
@@ -3,13 +3,13 @@
 function valid() {
     // document.getElementById("button").className='button disabled';
 
-    var team_name = document.forms['signup']['team_name'].value;
-    var pass = document.forms['signup']['pass'].value;
-    var cpass = document.forms['signup']['cpass'].value;
-    var email1 = document.forms['signup']['email1'].value;
-    var mob1 = document.forms['signup']['mob1'].value;
-    var manager1 = document.forms['signup']['manager1'].value;
-    if (team_name.length != 0 || pass.length != 0 || cpass.length != 0 || email1.length != 0 || mob1.length != 0 || manager1.length != 0) {
-        document.getElementById('button').className = 'button';
-    }
+    var team_name = document.getElementsByName('team_name').value;
+    var pass = document.getElementsByName('pass').value;
+    var cpass = document.getElementsByName('cpass').value;
+    var email1 = document.getElementsByName('email1').value;
+    var mob1 = document.getElementsByName('mob1').value;
+    var manager1 = document.getElementsByName('manager1').value;
+    document.getElementById('fill').style.display = 'none';
+    document.getElementById('cpass').style.display = 'none';
+
 }
