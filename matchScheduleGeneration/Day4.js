e5bb84e88d028efe178112644188057decc59d57
Kashish Singhal <singhal2.kashish@gmail.com>
2014-08-23 17:04:04 +0530

    Added Copyright Notices to all files

--- matchScheduleGeneration/Day4.js
+++ matchScheduleGeneration/Day4.js
@@ -2,7 +2,23 @@
  * Created by Kashish Singhal <singhal2.kashish@gmail.com> on 11/8/14.
  */
 
-
+/*
+ *  GraVITas Premier League
+ *  Copyright (C) 2014  IEEE Computer Society - VIT Student Chapter <ieeecs@vit.ac.in>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 var MongoClient = require('mongodb').MongoClient;
 
 var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/GPL';
--- mongoSquad.js
+++ mongoSquad.js
@@ -3,9 +3,23 @@
  */
 
 
-// i dont know much about mongo db and database connectivity.
-    // please let me know, where i have made a mistake, so that i can correct the changes... :)
-
+/*
+ *  GraVITas Premier League
+ *  Copyright (C) 2014  IEEE Computer Society - VIT Student Chapter <ieeecs@vit.ac.in>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 var MongoClient = require('mongodb').MongoClient;
 
