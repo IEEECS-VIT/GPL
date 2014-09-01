9fba06a3057ea67009f7083cdc05bbd8d8e88298
aneesh-neelam <neelam.aneesh@gmail.com>
2014-09-01 15:43:11 +0530

    Implemented static error pages Hosted the error pages on App Engine, using the Python SDK

---
+++ external/error-pages/README.md
@@ -0,0 +1,15 @@
+GraVITas Premier League: Error Pages
+====================================
+
+This is the static error page(s)
+
+For when GraVITas Premier League crashes or is taken down for maintenance
+
+#### Instructions:
+###### Install Pythono 2.7.x
+###### Install App Engine SDK for Python
+###### Deploy to Google App Engine
+    $ appcfg.py update error-pages/
+
+##### External Requirements
+* None
---
+++ external/error-pages/main.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+"""
+  GraVITas Premier League
+  Copyright (C) 2014  IEEE Computer Society - VIT Student Chapter <ieeecs@vit.ac.in>
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import os
+import webapp2
+import jinja2
+
+# This is required to use Jinja2
+JINJA_ENVIRONMENT = jinja2.Environment(
+loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+extensions=['jinja2.ext.autoescape'],
+autoescape=True)
+
+class ErrorHandler(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('templates/error.html')
+        self.response.write(template.render())
+
+
+class MaintenanceHandler(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('templates/maintenance.html')
+        self.response.write(template.render())
+
+
+app = webapp2.WSGIApplication([
+    ('/error', ErrorHandler),
+    ('/maintenance', MaintenanceHandler),
+], debug=True)
