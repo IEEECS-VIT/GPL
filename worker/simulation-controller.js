906bdfb3e0f291807dba248136f8f1041806282e
aneesh-neelam <neelam.aneesh@gmail.com>
2014-09-18 22:00:03 +0530

    Revert Find Issue 1

--- worker/simulation-controller.js
+++ worker/simulation-controller.js
@@ -72,11 +72,7 @@ exports.initSimulation = function (day, masterCallback)
             };
             var getRating = function (err, doc)
             {
-                if (!doc)
-                {
-                    console.log(query);
-                }
-                else if (doc.team.length < 16 || doc.squad < 11)
+                if (doc.team.length < 16 || doc.squad < 11)
                 {
                     asyncCallback(err, doc);
                 }
