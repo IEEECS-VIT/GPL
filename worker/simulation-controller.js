f70ce53aed6a15f5d0d56dc5dbc1798aa793c83e
aneesh-neelam <neelam.aneesh@gmail.com>
2014-09-18 21:53:20 +0530

    Find Issue 2

--- worker/simulation-controller.js
+++ worker/simulation-controller.js
@@ -72,8 +72,11 @@ exports.initSimulation = function (day, masterCallback)
             };
             var getRating = function (err, doc)
             {
-                if (!doc) console.log(query);
-                if (doc.team.length < 16 || doc.squad < 11)
+                if (!doc)
+                {
+                    console.log(query);
+                }
+                else if (doc.team.length < 16 || doc.squad < 11)
                 {
                     asyncCallback(err, doc);
                 }
