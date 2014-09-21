7845f0e4444f00c02ef1dc51daa14ddeffb4ff4b
Kunal Nagpal <kunagpal@gmail.com>
2014-09-21 21:55:42 +0530

    db/mongo-matches match day collection adjustment

--- db/mongo-matches.js
+++ db/mongo-matches.js
@@ -66,7 +66,7 @@ exports.fetchPreviousMatch = function (doc1, doc2, callback)
                     break;
 
             }
-            collectionName='matchday6';
+            collectionName='matchday7';
             var collection = db.collection(collectionName);
             var onFetch = function (err, docs)
             {
