690cf7ca02c87fb9e1ac7588359dfd893d5e3dd0
Kunal Nagpal <kunagpal@gmail.com>
2014-09-24 12:40:55 +0530

    Round 2 match 1 commentary

--- db/mongo-matches.js
+++ db/mongo-matches.js
@@ -66,7 +66,7 @@ exports.fetchPreviousMatch = function (doc1, doc2, callback)
                     break;
 
             }
-            collectionName='matchday7';
+            collectionName='matchday1';
             var collection = db.collection(collectionName);
             var onFetch = function (err, docs)
             {
