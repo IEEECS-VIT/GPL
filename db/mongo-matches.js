fe4bf9642e867b509f4d7b7c1d7ed6eda73bafc5
Kunal Nagpal <kunagpal@gmail.com>
2014-09-29 01:21:57 +0530

    Round 3, match 5 commentary

--- db/mongo-matches.js
+++ db/mongo-matches.js
@@ -66,7 +66,7 @@ exports.fetchPreviousMatch = function (doc1, doc2, callback)
                     break;
 
             }
-            collectionName='matchday4';   // Collection controller for match day information. To be changed before each match
+            collectionName='matchday5';   // Collection controller for match day information. To be changed before each match
             var collection = db.collection(collectionName);
             var onFetch = function (err, docs)
             {
