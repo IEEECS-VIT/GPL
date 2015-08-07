683e4853eaf944bdcc0dcb2e3152033b35f3012d
saurabhmathur96 <saurabhmathur96@gmail.com>
2015-08-07 16:15:56 +0530

    update landing page

---
+++ public/javascript/main.js
@@ -0,0 +1,24 @@
+$( "#navbar" ).load( "/html/navbar.html");
+$( "#footer" ).load( "/html/footer.html");
+
+$(document).ready(function () {
+
+ 	sticky_nav = function() {
+    $('#navbar-wrapper').height($("#navbar").height());
+
+    var calculated_offset = 0; 
+    calculated_offset = $('#navbar').offset().top;
+
+
+    console.log(calculated_offset);
+    $('#navbar').affix({
+        offset: { top: calculated_offset }
+    });
+	}
+	sticky_nav();
+	$(window).on('resize', function(){
+
+	$('#navbar').data('bs.affix').options.offset = $('#navbar').offset().top;
+	});
+
+});
