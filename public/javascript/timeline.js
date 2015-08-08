45b6437fdb4a5ca18ab23656bbb5ab71ed83db08
TanishaChawla <chawlatanisha20@gmail.com>
2015-08-09 00:22:25 +0530

    timeline stylesheet added

---
+++ public/javascript/timeline.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(".timeline").hide();
+    $("#head1").on("click", function()
+    {
+        $("#timeline1").slideToggle(500);
+        $("#timeline2").hide(500);
+    })
+    $("#head2").on("click", function()
+    {
+        $("#timeline2").slideToggle(500);
+        $("#timeline1").hide(500);
+    })
+})
