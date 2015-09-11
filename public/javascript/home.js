fe7854cbd3512b39522d55daedd11fe43b0ac7e4
ayushagarwal95 <agarwalayush161@gmail.com>
2015-09-11 15:07:01 +0530

    Toggling Fixed.

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -1,15 +1,16 @@
 /**
  * Created by user on 9/11/2015.
  */
-$(document).ready(function(){
-   $('.accordion-section-content').hide();
-    var current= $('.player-name.accordion-section-title').attr('href');
-    $('.accordion-section-title ').on("click",function(){
-        $(".accordion-section-content").slideToggle();
-        var h= $(".player").css("height");
-        if(h=='80px')
-            $(".player").animate({'height':'60px'});
+$(document).ready(function () {
+    $('.accordion-section-content').hide();
+    $('.accordion-section-title').on("click", function () {
+        var current = $(this).attr('href');
+        $(current).slideToggle();
+        var player_id = current.substr(1);
+        var h = $("#box" + player_id).css("height");
+        if (h === '80px')
+            $("#box" + player_id).animate({'height': '60px'});
         else
-            $(".player").animate({'height':'80px'});
+            $("#box" + player_id).animate({'height': '80px'});
     });
 });
