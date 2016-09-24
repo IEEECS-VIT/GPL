d6f88cd50a2eb60ffc39ce14b6bcaa00518e182f
kunagpal <kunagpal@gmail.com>
2016-09-24 17:45:36 +0530

    Structural tests

---
+++ test/unit/auth.js
@@ -0,0 +1,47 @@
+/*
+ *  graVITas Premier League <gravitaspremierleague@gmail.com>
+ *  Copyright (C) 2014  IEEE Computer Society - VIT Student Chapter <ieeecs@vit.ac.in>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// var assert = require("assert");
+
+describe("Authentication tests:", function() {
+    describe("Forget related actions:", function() {
+        it("Forgot user");
+
+        it("Forgot password");
+    });
+
+    describe("Login actions:", function() {
+        it("Social login checks");
+
+        it("Local login checks");
+
+        it("Admin login checks");
+    });
+
+    describe("Registration checks:", function() {
+        it("Social registration checks");
+
+        it("Local registration checks");
+    });
+
+    describe("Logout checks:", function() {
+        it("Admin logout");
+
+        it("User logout");
+    });
+});
