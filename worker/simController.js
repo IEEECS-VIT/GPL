75a3754c1d9a36dc0ff6c22e5cc91985882157bb
kunagpal <kunagpal@gmail.com>
2016-09-25 00:22:27 +0530

    Linted Simulation controller

--- worker/simController.js
+++ worker/simController.js
@@ -16,171 +16,133 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-var i;
-var log;
-var match;
-var stats;
-var points = 0;
-var onGetRating;
-var parallelTasks;
-var async = require("async");
-var path = require("path").join;
-var helper = require(path(__dirname, "simControlHelper"));
-var email = require(path(__dirname, "..", "utils", "email", "email"));
+var i,
+	match,
+	stats,
+	points = 0,
+	onGetRating,
+	parallelTasks,
+	async = require("async"),
+	path = require("path").join,
+	helper = require(path(__dirname, "simControlHelper"));
+	// email = require(path(__dirname, "..", "utils", "email", "email"));
 
-if(!process.env.NODE_ENV)
-{
-    require("dotenv").load({path : path(__dirname, "..", ".env")});
-}
-
-if (process.env.LOGENTRIES_TOKEN)
-{
-    log = require("node-logentries").logger({token: process.env.LOGENTRIES_TOKEN});
+if (!process.env.NODE_ENV) {
+	require("dotenv").load({ path: path(__dirname, "..", ".env") }); // eslint-disable-line global-require
 }
 
 match = process.env.MATCH;
-//email.match.header.bcc = [];
+// email.match.header.bcc = [];
 
-exports.initSimulation = function (day, masterCallback)
-{
-    var forEachMatch = function (matchDoc, callback)
-    {
-        var getTeamDetails = function (query, asyncCallback)
-        {
-            var getRating = function (err, userDoc)
-            {
-                if(err)
-                {
-                    throw err;
-                }
-                if (userDoc.squad.length < 11)
-                {
-                    userDoc.ratings = [];
-                    asyncCallback(err, userDoc);
-                }
-                else
-                {
-                    onGetRating = helper.onGetRating(userDoc, asyncCallback);
-                    userDoc.squad.push(userDoc.team.filter((elt) => {return elt > "d";})[0]);
-                    async.map(userDoc.squad, helper.getEachRating, onGetRating);
-                }
-            };
+exports.initSimulation = function (day, masterCallback) {
+	var forEachMatch = function (matchDoc, callback) {
+			var getTeamDetails = function (query, asyncCallback) {
+					var getRating = function (err, userDoc) {
+						if (err) {
+							throw err;
+						}
+						if (userDoc.squad.length < 11) {
+							userDoc.ratings = [];
+							asyncCallback(err, userDoc);
+						}
+						else {
+							onGetRating = helper.onGetRating(userDoc, asyncCallback);
+							userDoc.squad.push(userDoc.team.filter((elt) => { return elt > "d"; })[0]);
+							async.map(userDoc.squad, helper.getEachRating, onGetRating);
+						}
+					};
 
-            database.collection(match).find(query).limit(1).next(getRating);
-        };
+					global.database.collection(match).find(query).limit(1).next(getRating);
+				},
+				totals = function (newUserDoc) {
+					if (newUserDoc.scores[day - 1] > stats.daily.total.value) {
+						stats.daily.total = helper.dailyTotal(newUserDoc, day);
+					}
+					if (newUserDoc.highestTotal > stats.high.total.value) {
+						stats.high.total = helper.total(newUserDoc, "high");
+					}
+					if (newUserDoc.lowestTotal < stats.low.value) {
+						stats.low = helper.total(newUserDoc, "low");
+					}
+				},
+				nonBowler = function (newUserDoc) {
+					if (!newUserDoc.squad[i].match(/^b/) && !newUserDoc.squad[i].match(/^d/)) {
+						if (newUserDoc.stats[newUserDoc.squad[i]].recent[day - 1] > stats.daily.individual.value) {
+							stats.daily.individual = helper.dailyHigh(newUserDoc, i, day);
+						}
+						if (newUserDoc.stats[newUserDoc.squad[i]].high > stats.high.individual.value) {
+							stats.high.individual = helper.overallHigh(newUserDoc, i);
+						}
+						if (newUserDoc.stats[newUserDoc.squad[i]].runs > stats.orange.runs) {
+							stats.orange = helper.orangeCap(i, newUserDoc);
+						}
+					}
+				},
+				updateData = function (newData) {
+					var updateUser = function (newUserDoc, asyncCallback) {
+							if (newUserDoc.squad.length) {
+								stats.general = helper.generalStats(stats.general, newUserDoc, day);
+								totals(newUserDoc);
 
-        parallelTasks = helper.teamParallelTasks(getTeamDetails, matchDoc);
+								for (i = 0; i < newUserDoc.squad.length; ++i) {
+									nonBowler(newUserDoc);
 
-        var totals = function(newUserDoc)
-        {
-            if(newUserDoc.scores[day - 1] > stats.daily.total.value)
-            {
-                stats.daily.total = helper.dailyTotal(newUserDoc, day);
-            }
-            if(newUserDoc.highestTotal > stats.high.total.value)
-            {
-                stats.high.total = helper.total(newUserDoc, "high");
-            }
-            if(newUserDoc.lowestTotal < stats.low.value)
-            {
-                stats.low = helper.total(newUserDoc, "low");
-            }
-        };
 
-        var nonBowler = function(newUserDoc)
-        {
-            if (!newUserDoc.squad[i].match(/^b/) && !newUserDoc.squad[i].match(/^d/))
-            {
-                
-                if(newUserDoc.stats[newUserDoc.squad[i]].recent[day - 1] > stats.daily.individual.value)
-                {
-                    stats.daily.individual = helper.dailyHigh(newUserDoc, i, day);
-                }
-                if(newUserDoc.stats[newUserDoc.squad[i]].high > stats.high.individual.value)
-                {
-                    stats.high.individual = helper.overallHigh(newUserDoc, i);
-                }
-                if(newUserDoc.stats[newUserDoc.squad[i]].runs > stats.orange.runs)
-                {
-                    stats.orange = helper.orangeCap(i, newUserDoc);
-                }
-            }
-        };
+									if (newUserDoc.squad[i].match(/^[^a]/) &&
+                                    !newUserDoc.squad[i].match(/^d/) &&
+                                    newUserDoc.stats[newUserDoc.squad[i]].wickets > stats.purple.wickets) {
+										stats.purple = helper.purpleCap(i, newUserDoc);
+									}
+								}
+							}
 
-        var updateData = function (newData)
-        {
-            var updateUser = function (newUserDoc, asyncCallback)
-            {
-                if(newUserDoc.squad.length)
-                {
-                    stats.general = helper.generalStats(stats.general, newUserDoc, day);
-                    totals(newUserDoc);
+							delete newUserDoc.f;
+							delete newUserDoc.s;
+							delete newUserDoc.names;
 
-                    for (i = 0; i < newUserDoc.squad.length; ++i)
-                    {
-                        nonBowler(newUserDoc);
-                        
-                        
-                        if (newUserDoc.squad[i].match(/^[^a]/) && !newUserDoc.squad[i].match(/^d/) && newUserDoc.stats[newUserDoc.squad[i]].wickets > stats.purple.wickets)
-                        {
-                            stats.purple = helper.purpleCap(i, newUserDoc);
-                        }
-                    }
-                }
+                            // eslint-disable-next-line no-underscore-dangle
+							global.database.collection(match).updateOne({ _id: newUserDoc._id }, newUserDoc,
+                                asyncCallback);
+						},
+						updateMatch = function (newMatchDoc, asyncCallback) {
+							if (points < (newMatchDoc.MoM.points || 0)) {
+								points = newMatchDoc.MoM.points;
+								stats.daily.MoM = newMatchDoc.MoM;
+							}
 
-                delete newUserDoc.f;
-                delete newUserDoc.s;
-                delete newUserDoc.names;
+							global.database.collection(`matchday${day}`)// eslint-disable-next-line no-underscore-dangle
+                                .updateOne({ _id: newMatchDoc._id }, newMatchDoc, asyncCallback);
+						},
+						parallelTasks2 = helper.userParallelTasks(newData, updateUser, updateMatch);
 
-                database.collection(match).updateOne({_id: newUserDoc._id}, newUserDoc, asyncCallback);
-            };
+                    // eslint-disable-next-line no-underscore-dangle
+					console.log(`${newData.team1._id} v ${newData.team2._id} (Updating match ${newData.match._id})`);
+					async.parallel(parallelTasks2, callback);
+				},
+				onTeamDetails = helper.onTeamDetails(matchDoc, updateData);
 
-            var updateMatch = function (newMatchDoc, asyncCallback)
-            {
-                if (points < (newMatchDoc.MoM.points || 0))
-                {
-                    points = newMatchDoc.MoM.points;
-                    stats.daily.MoM = newMatchDoc.MoM;
-                }
+			parallelTasks = helper.teamParallelTasks(getTeamDetails, matchDoc);
+			async.parallel(parallelTasks, onTeamDetails);
+		},
+		onFinish = function (err, results) {
+			if (err) {
+				throw err;
+			}
 
-                database.collection("matchday" + day).updateOne({_id: newMatchDoc._id}, newMatchDoc, asyncCallback);
-            };
+			var onUpdate = helper.onUpdate(results, masterCallback);
 
-            var parallelTasks2 = helper.userParallelTasks(newData, updateUser, updateMatch);
+			global.database.collection("stats").updateOne({ _id: "stats" }, { $set: stats }, onUpdate);
+		},
+		forAllMatches = helper.forAllMatches(forEachMatch, onFinish),
+		onGetInfo = function (err, doc) {
+			if (err) {
+				throw err;
+			}
 
-            console.log(newData.team1._id + " vs " + newData.team2._id + " (Match " + newData.match._id + ") is now being updated");
-            async.parallel(parallelTasks2, callback);
-        };
+			stats = doc;
+			stats.daily.total.value = stats.daily.individual.value = stats.daily.MoM.points = 0;
+			helper.getAllMatches(day, forAllMatches);
+		};
 
-        var onTeamDetails = helper.onTeamDetails(matchDoc, updateData);
-
-        async.parallel(parallelTasks, onTeamDetails);
-    };
-
-    var onFinish = function (err, results)
-    {
-        if(err)
-        {
-            throw err;
-        }
-
-        var onUpdate = helper.onUpdate(results, masterCallback);
-        database.collection("stats").updateOne({_id: "stats"}, {$set: stats}, onUpdate);
-    };
-
-    var forAllMatches = helper.forAllMatches(forEachMatch, onFinish);
-
-    var onGetInfo = function (err, doc)
-    {
-        if (err)
-        {
-            throw err;
-        }
-
-        stats = doc;
-        stats.daily.total.value = stats.daily.individual.value = stats.daily.MoM.points = 0;
-        helper.getAllMatches(day, forAllMatches);
-    };
-
-    database.collection("stats").find().limit(1).next(onGetInfo);
+	global.database.collection("stats").find().limit(1).next(onGetInfo);
 };
