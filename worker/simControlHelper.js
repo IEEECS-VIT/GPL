ce0afff94527a5ef49d08252d4804424a5c4a1ce
kunagpal <kunagpal@gmail.com>
2016-09-25 00:03:40 +0530

    Linted simControl helper

--- worker/simControlHelper.js
+++ worker/simControlHelper.js
@@ -16,190 +16,153 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-var simData;
-var async = require("async");
-var path = require("path").join;
-var days = [1, 2, 3, 4, 5, 6, 7];
-var simulate = require(path(__dirname, "simulation")).simulate;
+var simData,
+	async = require("async"),
+	path = require("path").join,
+	days = [1, 2, 3, 4, 5, 6, 7],
+	simulate = require(path(__dirname, "simulation")).simulate;
 
-exports.onGetRating = function(userDoc, asyncCallback)
-{
-    return (err, results) => {
-        if(err)
-        {
-            throw err;
-        }
+exports.onGetRating = function (userDoc, asyncCallback) {
+	return (err, results) => {
+		if (err) {
+			throw err;
+		}
 
-        userDoc.ratings = results;
-        asyncCallback(err, userDoc);
-    };
+		userDoc.ratings = results;
+		asyncCallback(err, userDoc);
+	};
 };
 
-exports.getAllMatches = function (day, callback)
-{
-    if(days.indexOf(day) === -1)
-    {
-        throw "Invalid Day";
-    }
+exports.getAllMatches = function (day, callback) {
+	if (days.indexOf(day) === -1) {
+		throw new Error("Invalid Day");
+	}
 
-    database.collection("matchday" + day).find().toArray(callback)
+	global.database.collection(`matchday${day}`).find().toArray(callback);
 };
 
-exports.teamParallelTasks = function(getTeamDetails, matchDoc)
-{
-    return {
-        "team1": function (asyncCallback)
-        {
-            getTeamDetails({"teamNo": matchDoc.Team_1}, asyncCallback);
-        },
-        "team2": function(asyncCallback)
-        {
-            getTeamDetails({"teamNo": matchDoc.Team_2}, asyncCallback);
-        }
-    };
+exports.teamParallelTasks = function (getTeamDetails, matchDoc) {
+	return {
+		team1: function (asyncCallback) { getTeamDetails({ teamNo: matchDoc.Team_1 }, asyncCallback); },
+		team2: function (asyncCallback) { getTeamDetails({ teamNo: matchDoc.Team_2 }, asyncCallback); }
+	};
 };
 
-exports.userParallelTasks = function(newData, updateUser, updateMatch)
-{
-    return [
-        function(asyncCallback)
-        {
-            updateUser(newData.team1, asyncCallback);
-        },
-        function(asyncCallback)
-        {
-            updateUser(newData.team2, asyncCallback);
-        },
-        function(asyncCallback)
-        {
-            updateMatch(newData.match, asyncCallback);
-        }
-    ];
+exports.userParallelTasks = function (newData, updateUser, updateMatch) {
+	return [
+		function (asyncCallback) { updateUser(newData.team1, asyncCallback); },
+		function (asyncCallback) { updateUser(newData.team2, asyncCallback); },
+		function (asyncCallback) { updateMatch(newData.match, asyncCallback); }
+	];
 };
 
-exports.generalStats = function(general, newUserDoc, day)
-{
-    return {
-        "sixes": general.sixes + newUserDoc.s || 0,
-        "fours": general.fours + newUserDoc.f || 0,
-        "runs": general.runs + newUserDoc.scores[day - 1] || 0,
-        "overs": general.overs + newUserDoc.overs[day - 1] || 0,
-        "wickets": general.wickets + newUserDoc.wickets[day - 1] || 0
-    };
+exports.generalStats = function (general, newUserDoc, day) {
+	return {
+		sixes: general.sixes + newUserDoc.s || 0,
+		fours: general.fours + newUserDoc.f || 0,
+		runs: general.runs + newUserDoc.scores[day - 1] || 0,
+		overs: general.overs + newUserDoc.overs[day - 1] || 0,
+		wickets: general.wickets + newUserDoc.wickets[day - 1] || 0
+	};
 };
 
-exports.total = function(newUserDoc, mode)
-{
-    return {
-        "team": newUserDoc._id,
-        "value": newUserDoc[`${mode}estTotal`]
-    };
+exports.total = function (newUserDoc, mode) {
+	return {
+		team: newUserDoc._id, // eslint-disable-line no-underscore-dangle
+		value: newUserDoc[`${mode}estTotal`]
+	};
 };
 
-exports.dailyTotal = function(newUserDoc, day)
-{
-    return {
-        "team": newUserDoc._id,
-        "value": newUserDoc.scores[day - 1]
-    };
+exports.dailyTotal = function (newUserDoc, day) {
+	return {
+		team: newUserDoc._id, // eslint-disable-line no-underscore-dangle
+		value: newUserDoc.scores[day - 1]
+	};
 };
 
-exports.dailyHigh = function(newUserDoc, i, day)
-{
-    return {
-        "team": newUserDoc._id,
-        "player": newUserDoc.names[i] || "",
-        "value": newUserDoc.stats[newUserDoc.squad[i]].recent[day - 1]
-    };
+exports.dailyHigh = function (newUserDoc, i, day) {
+	return {
+		team: newUserDoc._id, // eslint-disable-line no-underscore-dangle
+		player: newUserDoc.names[i] || "",
+		value: newUserDoc.stats[newUserDoc.squad[i]].recent[day - 1]
+	};
 };
 
-exports.overallHigh = function(newUserDoc, i)
-{
-    return {
-        "team": newUserDoc._id,
-        "player": newUserDoc.names[i] || "",
-        "value": newUserDoc.stats[newUserDoc.squad[i]].high
-    };
+exports.overallHigh = function (newUserDoc, i) {
+	return {
+		team: newUserDoc._id, // eslint-disable-line no-underscore-dangle
+		player: newUserDoc.names[i] || "",
+		value: newUserDoc.stats[newUserDoc.squad[i]].high
+	};
 };
 
-exports.purpleCap = function(i, newUserDoc)
-{
-    return {
-        "team": newUserDoc._id,
-        "player": newUserDoc.names[i] || "",
-        "economy": newUserDoc.stats[newUserDoc.squad[i]].economy,
-        "wickets": newUserDoc.stats[newUserDoc.squad[i]].wickets,
-        "bowlAverage": newUserDoc.stats[newUserDoc.squad[i]].bowlAverage,
-        "ballsBowled": newUserDoc.stats[newUserDoc.squad[i]].ballsBowled,
-        "runsConceded": newUserDoc.stats[newUserDoc.squad[i]].runsConceded,
-        "bowlStrikeRate": newUserDoc.stats[newUserDoc.squad[i]].bowlStrikeRate
-    };
+exports.purpleCap = function (i, newUserDoc) {
+	return {
+		team: newUserDoc._id, // eslint-disable-line no-underscore-dangle
+		player: newUserDoc.names[i] || "",
+		economy: newUserDoc.stats[newUserDoc.squad[i]].economy,
+		wickets: newUserDoc.stats[newUserDoc.squad[i]].wickets,
+		bowlAverage: newUserDoc.stats[newUserDoc.squad[i]].bowlAverage,
+		ballsBowled: newUserDoc.stats[newUserDoc.squad[i]].ballsBowled,
+		runsConceded: newUserDoc.stats[newUserDoc.squad[i]].runsConceded,
+		bowlStrikeRate: newUserDoc.stats[newUserDoc.squad[i]].bowlStrikeRate
+	};
 };
 
-exports.orangeCap = function(i, newUserDoc)
-{
-    return {
-        "team": newUserDoc._id,
-        "player": newUserDoc.names[i] || "",
-        "high": newUserDoc.stats[newUserDoc.squad[i]].high,
-        "runsScored": newUserDoc.stats[newUserDoc.squad[i]].runsScored,
-        "ballsFaced": newUserDoc.stats[newUserDoc.squad[i]].ballsFaced,
-        "batAverage": newUserDoc.stats[newUserDoc.squad[i]].batAverage,
-        "batStrikeRate": newUserDoc.stats[newUserDoc.squad[i]].batStrikeRate
-    };
+exports.orangeCap = function (i, newUserDoc) {
+	return {
+		team: newUserDoc._id, // eslint-disable-line no-underscore-dangle
+		player: newUserDoc.names[i] || "",
+		high: newUserDoc.stats[newUserDoc.squad[i]].high,
+		runsScored: newUserDoc.stats[newUserDoc.squad[i]].runsScored,
+		ballsFaced: newUserDoc.stats[newUserDoc.squad[i]].ballsFaced,
+		batAverage: newUserDoc.stats[newUserDoc.squad[i]].batAverage,
+		batStrikeRate: newUserDoc.stats[newUserDoc.squad[i]].batStrikeRate
+	};
 };
 
-exports.makeData = function(results, matchDoc)
-{
-    return {
-        "team": [results.team1, results.team2],
-        "match": matchDoc
-    };
+exports.makeData = function (results, matchDoc) {
+	return {
+		team: [results.team1, results.team2],
+		match: matchDoc
+	};
 };
 
-exports.getEachRating = function (elt, subCallback)
-{
-    database.collection("players").find({"_id": elt}).limit(1).next(subCallback);
+exports.getEachRating = function (elt, subCallback) {
+	global.database.collection("players").find({ _id: elt }).limit(1).next(subCallback);
 };
 
-exports.onTeamDetails = function(matchDoc, updateData)
-{
-    return (err, results) => {
-        if(err)
-        {
-            throw err;
-        }
+exports.onTeamDetails = function (matchDoc, updateData) {
+	return (err, results) => {
+		if (err) {
+			throw err;
+		}
 
-        simData =
-        {
-            "team": [results.team1, results.team2],
-            "match": matchDoc
-        };
+		simData = {
+			team: [results.team1, results.team2],
+			match: matchDoc
+		};
 
-        simulate(simData, updateData);
-    };
+		simulate(simData, updateData);
+	};
 };
 
-exports.onUpdate = function(results, masterCallback)
-{
-    return (err) => {
-        if(err)
-        {
-            throw err;
-        }
+exports.onUpdate = function (results, masterCallback) {
+	return (err) => {
+		if (err) {
+			throw err;
+		}
 
-        masterCallback(null, results);
-    };
+		masterCallback(null, results);
+	};
 };
 
-exports.forAllMatches = function(forEachMatch, onFinish)
-{
-    return (err, docs) => {
-        if (err)
-        {
-            throw err;
-        }
+exports.forAllMatches = function (forEachMatch, onFinish) {
+	return (err, docs) => {
+		if (err) {
+			throw err;
+		}
 
-        async.map(docs, forEachMatch, onFinish);
-    };
+		async.map(docs, forEachMatch, onFinish);
+	};
 };
